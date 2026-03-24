b4f6ba8939231021c9ebf5a30f9228291cc5318c
kobia <josephacquah@Josephs-MacBook-Pro.local>
2026-03-24 04:16:33 -0230

    test: integrate cypress-testing-library and fix baseUrl config
    - Add @testing-library/cypress to package.json and support/commands.js
    
    - Configure baseUrl in cypress.config.js and update tests to use cy.visit('/')
    
    - Refactor tests to use Testing Library queries (findByText, findByPlaceholderText)
    
    - Add cypress.env.json to .gitignore for security
    
    - Comment out legacy assertions and add async behavior demonstration

--- cypress/e2e/1-getting-started/myFirstTest.cy.js
+++ cypress/e2e/1-getting-started/myFirstTest.cy.js
@@ -1,23 +1,32 @@
 context('My First Test', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:8080/todo')
+    cy.visit('/')
   })
 
-  it('has an h1 on the page', () => {
-    cy.get('h1').should('exist')
-  })
+  // it('has an h1 on the page', () => {
+  //   cy.get('h1').should('exist')
+  // })
 
-  it('renders the correct h1 text', () => {
-    cy.get('h1').should('contain.text', 'todos')
-  })
+  // it('renders the correct h1 text', () => {
+  //   cy.get('h1').should('contain.text', 'todos')
+  // })
 
-  it('renders a todo item under the input', () => {
-    cy.get('.view').eq(1).find('label').should('exist')
-  })
+  // it('renders a todo item under the input', () => {
+  //   cy.get('.view').eq(1).find('label').should('exist')
+  // })
 
-  it('renders a section with the correct elements', () => {
-    cy.get('.view').eq(1).within(() => {
-      cy.get('label').should('exist')
-    })
+  // it('renders a section with the correct elements', () => {
+  //   cy.get('.view').eq(1).within(() => {
+  //     cy.get('label').should('exist')
+  //   })
+  // })
+
+  it('currently renders the todo website link', () => {
+    cy.findByText('todos').should('exist')
+    cy.findByPlaceholderText('What needs to be done?').type('exist')
+    // eslint-disable-line no-console
+    cy.wait(5000)
+    // non-cypress code runs first
+    console.log('first to execute')
   })
 })
